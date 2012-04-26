b0aa703c8f3fef44f16d2ea179c37011121e72d8
Terry May <terry@detroitlabs.com>
2012-04-25 20:19:42 -0400

    hotwired app.js to render the index template with jade

--- app.js
+++ app.js
@@ -3,21 +3,16 @@
  * Module dependencies.
  */
 
-var express = require('express')
-  , routes = require('./routes');
+var express = require('express');
+var app = express.createServer();
+var jade = require('jade');
 
-var app = module.exports = express.createServer();
-
+//var app = module.exports = express.createServer();
+app.register('.html', require('jade'));
 // Configuration
 
-app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.set('view engine', '.');
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
-});
+app.use(express.static(__dirname + '/public'));
+app.use(express.bodyParser());
 
 app.configure('development', function(){
   app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
@@ -29,7 +24,9 @@ app.configure('production', function(){
 
 // Routes
 
-app.get('/', routes.index);
+app.get('/', function (req, res) {
+   res.render('index.jade', { title: 'Express' })
+});
 
 app.listen(3000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
